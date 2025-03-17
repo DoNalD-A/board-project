a42fea77317fb911a6e8d4ef8a24984911e01b07
DoNalD-A <thdlstjrj@naver.com>
2025-03-17 23:10:19 +0900

    TestDto 추가

---
+++ src/main/java/basic/boardproject/dto/TestDto.java
@@ -0,0 +1,15 @@
+package basic.boardproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TestDto {
+    private String user_id;
+    private String password;
+    private String email;
+
+}
