0387dcb05910168d9fab057bed43d6ff489f558c
DoNalD-A <thdlstjrj@naver.com>
2025-05-11 22:54:06 +0900

    UserServiceTest 추가

---
+++ src/main/java/basic/boardproject/service/UserServiceTest.java
@@ -0,0 +1,24 @@
+package basic.boardproject.service;
+
+import basic.boardproject.domain.Users;
+import basic.boardproject.dto.UserNewDto;
+import basic.boardproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceTest {
+
+    private final UserRepository userRepository;
+
+    //유저 생성
+    public void userSave(UserNewDto userNewDto) {
+        Users users = new Users(userNewDto);
+        if (userRepository.existsById(users.getId())) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+        userRepository.save(users);
+    }
+
+}
